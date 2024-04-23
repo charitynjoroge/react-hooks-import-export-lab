3e06fb5f715af6b449780efbfe53274110a68624
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-04-23 14:12:24 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import About from "./About";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+      <Home/>
+      <About/>
     </div>
   );
 }
